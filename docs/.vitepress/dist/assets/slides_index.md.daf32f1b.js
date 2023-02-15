ea2a639ac48dc7bf8b64f4d33547f64d6cfd2f64
jannes lambrecht <janneslambrecht@hotmail.com>
2023-02-15 13:01:22 +0100

    [WIP]

---
+++ docs/.vitepress/dist/assets/slides_index.md.daf32f1b.js
@@ -0,0 +1,3 @@
+import{_ as e,c as t,o as s,b as o}from"./app.c84685b2.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"slides/index.md"}'),n={name:"slides/index.md"},i=o("iframe",{src:"/docs/slides/slides/index.html",style:{position:"fixed",top:"0",left:"0",bottom:"0",right:"0",width:"100%",height:"100%",border:"none",margin:"0",padding:"0",overflow:"hidden","z-index":"999999"}},`
+    Your browser doesn't support iframes
+`,-1),r=[i];function d(a,c,l,p,_,m){return s(),t("div",null,r)}const x=e(n,[["render",d]]);export{h as __pageData,x as default};
